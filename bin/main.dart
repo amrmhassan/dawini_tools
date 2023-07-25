ffa24fb5a1cb4dd8ba18b78a24d3a8386ca1a860
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2023-07-25 11:47:28 +0300

    downloading pharmacies mified file

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 void main(List<String> args) async {
   Dio dio = Dio();
   await dio.download(
-      'https://firebasestorage.googleapis.com/v0/b/dawini2023.appspot.com/o/v5%2Fpharmacies.json?alt=media&token=4ec05314-1570-4cb7-8bdf-bccb8b0e07cc',
-      './data/pharmacies.json');
+      'https://firebasestorage.googleapis.com/v0/b/dawini2023.appspot.com/o/v5%2Flist_pharmacies_minified.json?alt=media&token=2d45443c-e6f4-40f5-82de-2f0d671eca70',
+      './data/list_pharmacies_minified.json');
   print('downloaded');
 }
