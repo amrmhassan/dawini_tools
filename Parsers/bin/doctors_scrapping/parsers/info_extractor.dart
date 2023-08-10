b0f52bec16e9d583b112d91d9188c2affe7e0ab1
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2023-08-10 08:45:07 +0300

    doctor info extractor

--- Parsers/bin/doctors_scrapping/parsers/info_extractor.dart
+++ Parsers/bin/doctors_scrapping/parsers/info_extractor.dart
@@ -1,16 +1,92 @@
-import '../constants/urls.dart';
-import '../constants/variables.dart';
+import 'package:html/dom.dart';
 
-class InfoExtractor {
-  Future<int> pagesNumber() async {
-    String url = Urls.mainUrl;
+class DoctorInfo {
+  final Element _document;
+  const DoctorInfo(this._document);
 
-    var document = documentParser.get(url);
-    var element = document
-        .querySelector("#search-doctors-page__Pagination-page-no--1035");
-    String value = element?.text ?? '';
-    print(value);
-
-    return 0;
+  String? get name {
+    return _document
+        .querySelector(
+            "span > div.CommonStylesstyle__ColDirection-sc-1vkcu2o-1.dfaYOD > div.Gridstyle__ColStyle-sc-1lgtuty-0.cIJIvF > a > h4")
+        ?.text;
   }
+
+  String? get title {
+    return _document
+        .querySelector(
+            "span > div.CommonStylesstyle__ColDirection-sc-1vkcu2o-1.dfaYOD > div.Gridstyle__ColStyle-sc-1lgtuty-0.cIJIvF > p")
+        ?.text;
+  }
+
+  int? get visitors {
+    String? content = _document
+        .querySelector(
+            "#doctor-card__29104 > span > div.CommonStylesstyle__ColDirection-sc-1vkcu2o-1.dfaYOD > div.Gridstyle__ColStyle-sc-1lgtuty-0.cIJIvF > div.DoctorCardstyle__DoctorRatingWrapper-sc-uptab2-9.bxsqEC > div.DoctorCardstyle__RatingContainer-sc-uptab2-10.eZRYZm")
+        ?.text;
+    if (content == null) return null;
+    content = clean(content);
+    content = content!.replaceAll('التقييم العام من', '');
+    content = content.replaceAll('زاروا الدكتور', '');
+    content = content.trim();
+    int? visitors = arabicToNumber(content);
+    return visitors;
+  }
+
+  String? get specialization {
+    String? content = _document
+        .querySelector(
+            "span > div.CommonStylesstyle__ColDirection-sc-1vkcu2o-1.dfaYOD > div.Gridstyle__ColStyle-sc-1lgtuty-0.cIJIvF > span.DoctorCardstyle__HideOnMobile-sc-uptab2-0.wnblj > span.DoctorCardSubComponentsstyle__SpecialtyRowLimitDetailsWrapper-sc-1vq3h7c-21.jzapWu > span")
+        ?.text;
+    content = clean(content);
+    return content;
+  }
+
+  String? get place {
+    return _document
+        .querySelector(
+            "span > div.CommonStylesstyle__ColDirection-sc-1vkcu2o-1.dfaYOD > div.Gridstyle__ColStyle-sc-1lgtuty-0.cIJIvF > span.DoctorCardstyle__HideOnMobile-sc-uptab2-0.wnblj > span.DoctorCardstyle__Text-sc-uptab2-4.blwPZf")
+        ?.text;
+  }
+
+  int? get price {
+    var container = _document.querySelector(
+        "span > div.CommonStylesstyle__ColDirection-sc-1vkcu2o-1.dfaYOD > div.Gridstyle__ColStyle-sc-1lgtuty-0.cIJIvF > span.DoctorCardstyle__HideOnMobile-sc-uptab2-0.wnblj");
+    var line = container?.querySelector('span:nth-child(17)');
+    String? priceText = line?.text;
+    int? priceInt = arabicToNumber(priceText);
+    return priceInt;
+  }
+
+  String? get waiting {
+    return _document
+        .querySelector(
+            "span > div.CommonStylesstyle__ColDirection-sc-1vkcu2o-1.dfaYOD > div.Gridstyle__ColStyle-sc-1lgtuty-0.cIJIvF > span.DoctorCardstyle__HideOnMobile-sc-uptab2-0.wnblj > span.DoctorCardstyle__Text-sc-uptab2-4.lhhnfH")
+        ?.text;
+  }
+}
+
+String? clean(String? content) {
+  content = content?.replaceAll('\n', '');
+  content = content?.replaceAll(RegExp(r'\s{2,}'), ' ');
+  return content;
+}
+
+String _arabicNumbers = '٠١٢٣٤٥٦٧٨٩';
+int? arabicToNumber(String? arabic) {
+  if (arabic == null) return null;
+  arabic = arabic.trim();
+
+  String englishRes = '';
+  List<String> arabicList = _arabicNumbers.split('');
+  List<String> inWorkArabicNumbers = arabic.split('');
+  for (var number in inWorkArabicNumbers) {
+    int index = arabicList.indexOf(number);
+    if (index == -1) {
+      return null;
+    }
+    // here get the number index and add it to the english results
+    englishRes += index.toString();
+  }
+  int? finalNumber = int.tryParse(englishRes);
+  return finalNumber;
 }
--- Parsers/bin/doctors_scrapping/scrapping_runner.dart
+++ Parsers/bin/doctors_scrapping/scrapping_runner.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:html/dom.dart';
 import 'constants/variables.dart';
+import 'parsers/info_extractor.dart';
 
 class ScrappingRunner {
   final String? dataPath;
@@ -9,6 +12,25 @@ class ScrappingRunner {
   void run() {
     String content = fileReader.read(1, dataPath);
     Document document = documentParser.get(content);
-    print(document.body?.text);
+    Element? element = document.querySelector('[data-testid="doctor-card-0"]');
+    if (element == null) return;
+
+    DoctorInfo doctorInfo = DoctorInfo(element);
+    print(doctorInfo.name);
+    print(doctorInfo.title);
+    print(doctorInfo.specialization);
+    print(doctorInfo.visitors);
+    print(doctorInfo.waiting);
+    print(doctorInfo.price);
+
+    File file = File('test.txt');
+    file.writeAsStringSync('''
+${doctorInfo.name}
+${doctorInfo.title}
+${doctorInfo.specialization}
+${doctorInfo.place}
+${doctorInfo.visitors}
+${doctorInfo.waiting}
+${doctorInfo.price}''');
   }
 }
