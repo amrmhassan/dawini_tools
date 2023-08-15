44bbc77269e2974cceccf9bf510c3ea76aa2941d
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2023-08-15 09:10:12 +0300

    output doctors data link for linux updated

--- Parsers/bin/main.dart
+++ Parsers/bin/main.dart
@@ -10,7 +10,7 @@ import 'doctor_data_extractor/extractor.dart';
 void main(List<String> args) async {
   String folderPath = Platform.isWindows
       ? 'D:/Study And Work/Work/projects/flutter/Dart Mastery/dawini_tools/output_doctors/'
-      : '/home/ubuntu/downloads/dawini_tools/output_doctors/';
+      : '/home/ubuntu/downloads/dawini_tools/Parsers/bin/doctors_data_downloader/output_doctors/';
 
   String doctorsFilePath = Platform.isWindows
       ? 'D:/Study And Work/Work/projects/flutter/Dart Mastery/dawini_tools/Parsers/unDuplicatedIdsDoctors.json'
